e6bf3d37123ff2025554abb229284c89803d3d95
neeraj5696 <neerajumar5696@gmail.com>
2023-12-15 18:04:00 +0530

    first

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [count, setCount]= useState(0)
+
+   function hello(){
+    setCount(count+1)
+   }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+ <div> <h1 className='title' >BOOK-A-MOVIE</h1></div>
+<div className='md:flex gap-60' >
+  <div>
+   <div className='movie-row' >SELECT A MOVIE</div>
+ <div className='seat-row' >Select Time Slot</div>
+ <div className='slot-row' >Select Seats</div>
+ <div className='m-2' >Confirm Booking</div>
+</div>
+<div>Previous Booking Details</div>
+</div>
+   
+   </>
   )
 }
 
